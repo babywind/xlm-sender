2c0a205422a8bf94b5020bda5e4489788fafd3ec
leoxy <babywindxx@163.com>
2018-09-17 16:48:33 +0800

    修改字段名称

--- src/main/java/com/trade/sender/entity/WalletEntity.java
+++ src/main/java/com/trade/sender/entity/WalletEntity.java
@@ -6,39 +6,36 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 public class WalletEntity implements Serializable {
-	/**
-	 *
-	 */
+
 	private static final long serialVersionUID = 1L;
+
 	/**
 	 * 热钱包地址
 	 */
-	public String r_qbdz;
+	public String hwAddress;
+
 	/**
 	 * 热钱包私钥
 	 */
-	public String r_sy;
+	public String hwPrivateKey;
+
 	/**
 	 * 冷钱包地址
 	 */
-	public String l_qbdz;
+	public String cwAddress;
+
 	/**
 	 * 是否自动转入冷钱包
 	 */
 	public IsPass is;
+
 	/**
 	 * 大于多少自动转入
 	 */
-	public BigDecimal count = new BigDecimal(0);
+	public BigDecimal limit = new BigDecimal(0);
 
 	/**
 	 * 钱包服务器ip
 	 */
 	public String ip;
-
-	@Override
-	public String toString() {
-		return "WalletEntity [r_qbdz=" + r_qbdz + ", r_sy=" + r_sy + ", l_qbdz=" + l_qbdz + ", is=" + is + ", count="
-				+ count + ", ip=" + ip + "]";
-	}
 }
