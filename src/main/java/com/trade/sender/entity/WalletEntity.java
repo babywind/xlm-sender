f43700846477c5838e2556922545555aae669c75
leoxy <babywindxx@163.com>
2018-09-18 09:56:12 +0800

    update wallet entity

--- src/main/java/com/trade/sender/entity/WalletEntity.java
+++ src/main/java/com/trade/sender/entity/WalletEntity.java
@@ -35,7 +35,32 @@ public class WalletEntity implements Serializable {
 	public BigDecimal limit = new BigDecimal(0);
 
 	/**
+	 * 钱包地址
+	 */
+	public String address;
+
+	/**
+	 * 钱包私钥
+	 */
+	public String privateKey;
+
+	/**
 	 * 钱包服务器ip
 	 */
 	public String ip;
+
+	/**
+	 * 端口
+	 */
+	public String port;
+
+	/**
+	 * 服务器用户名
+	 */
+	public String serverName;
+
+	/**
+	 * 服务器密码
+	 */
+	public String serverPasswd;
 }
