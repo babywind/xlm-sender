497576ded820fde4ec168fafd4cf7e08eac5bc22
leoxy <babywindxx@163.com>
2018-09-18 17:08:26 +0800

    临时钱包创建 提币操作处理

--- src/main/java/com/trade/sender/service/achieve/XlmManageImpl.java
+++ src/main/java/com/trade/sender/service/achieve/XlmManageImpl.java
@@ -18,11 +18,14 @@ import org.stellar.sdk.*;
 import org.stellar.sdk.responses.AccountResponse;
 import org.stellar.sdk.responses.SubmitTransactionResponse;
 
+import java.io.InputStream;
 import java.math.BigDecimal;
+import java.net.URL;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 
 
 /**
@@ -38,22 +41,35 @@ public class XlmManageImpl extends AbstractManageService implements XlmManage {
 	// 账户最小余额XLM
 	final static private BigDecimal MIN_BALANCE = new BigDecimal(1.0000000);
 
+	final static private int ACCOUNT_LIMIT = 500;
+
 	final static private DecimalFormat XLM_FORMAT = new DecimalFormat("#.#######");
 
-	enum TRADE {TRANS_IN, TRANS_OUT}
+	private enum TRADE {
+		TRANS_IN, TRANS_OUT
+	}
 
 	// stellar(XLM)服务
 	private Server xlmServer;
 
+	private String url;
+
 	private XlmManageImpl(ServiceResource serviceResource) {
 		super(serviceResource);
 
-		Network.usePublicNetwork();
+		/*
+		 * 注意测试网络，生产网络切换。
+		 * 此处会影响转账交易的签名授权。
+		 */
+		Network.useTestNetwork();
+//		Network.usePublicNetwork();
 
 		try {
 			WalletEntity xlmWallet = this.getWalletInfo();
 
-			xlmServer = new Server(xlmWallet.ip);
+			url = xlmWallet.ip + ( StringHelper.isEmpty(xlmWallet.port) ? "" : (":" + xlmWallet.port) );
+
+			xlmServer = new Server(url);
 		} catch (Throwable throwable) {
 			throwable.printStackTrace();
 		}
@@ -126,8 +142,8 @@ public class XlmManageImpl extends AbstractManageService implements XlmManage {
 			result = "转入热钱包错误：" + e.getMessage();
 		}
 
-		execute(getConnection(P2PConst.DB_USER), "UPDATE  T6012_3 SET F05=?,F06=CURRENT_TIMESTAMP(),F08=?,F09=? WHERE F01=?",
-				IsPass.S, hash, 0, trade.id);
+		execute(getConnection(P2PConst.DB_USER), "UPDATE  T6012_3 SET F05=?,F06=CURRENT_TIMESTAMP(),F08=?,F09=? WHERE F01=?", IsPass.S, hash, 0, trade.id);
+
 		execute(getConnection(P2PConst.DB_USER), "UPDATE  T6012_4 SET F03=? WHERE F01=?", result, trade.id);
 
 		if (transAmount.compareTo(BigDecimal.ZERO) > 0) {
@@ -142,11 +158,13 @@ public class XlmManageImpl extends AbstractManageService implements XlmManage {
 		WalletEntity wallet = getWalletInfo();
 		if (IsPass.S == wallet.is && !StringHelper.isEmpty(wallet.cwAddress)) {
 			KeyPair hotKP = KeyPair.fromSecretSeed(MyCrypt.myDecode(wallet.hwPrivateKey));
-			KeyPair coldKP = KeyPair.fromAccountId(wallet.cwAddress);
 
-			// 获取钱包信息 校验钱包是否存在
+			KeyPair coldKP = KeyPair.fromAccountId(wallet.cwAddress);
+			// 校验钱包地址
+			xlmServer.accounts().account(coldKP);
+
+			// 获取钱包信息
 			AccountResponse hotWallet = xlmServer.accounts().account(hotKP);
-//			AccountResponse coldeWallet = xlmServer.accounts().account(coldKP);
 
 			BigDecimal balance = getXlmBalance(hotWallet);
 
@@ -165,7 +183,7 @@ public class XlmManageImpl extends AbstractManageService implements XlmManage {
 				// 提交事务
 				SubmitTransactionResponse transactionResponse = xlmServer.submitTransaction(transaction);
 				if (!transactionResponse.isSuccess()) {
-					throw new Exception("转入冷钱包错误：" + transactionResponse.getResultXdr());
+					throw new Exception("转入冷钱包错误：" + transactionResponse.getExtras().getResultCodes().getTransactionResultCode());
 				}
 			}
 		}
@@ -197,17 +215,146 @@ public class XlmManageImpl extends AbstractManageService implements XlmManage {
 
 	@Override
 	public void createAccount() throws Exception {
+		// 获取未使用的临时钱包数量
+		int cnt = selectInt(P2PConst.DB_USER, "SELECT COUNT(F01) FROM T6012_" + BJC + " WHERE F02 IS NULL AND F05 = 'F' ");
+
+		// 判断是否足够
+		if (cnt < ACCOUNT_LIMIT) {
+			for (int i = 0; i < ACCOUNT_LIMIT; i++) {
+				KeyPair newKey = KeyPair.random();
+
+				String address = newKey.getAccountId();
+				String privateKey = String.valueOf(newKey.getSecretSeed());
+
+				// 注册地址
+				String fbUrl = String.format(url + "/friendbot?addr=%s", address);
+
+				// 注册钱包信息
+				InputStream response = new URL(fbUrl).openStream();
+				new Scanner(response, "UTF-8").useDelimiter("\\A").next();
+
+				// 登记入表
+				execute(getConnection(P2PConst.DB_USER),
+						"INSERT INTO T6012_" + BJC + " (F03,F04,F05,F06) VALUES (?, ?, 'F', CURRENT_TIMESTAMP())",
+						address, MyCrypt.myEncode(privateKey)
+				);
+			}
+
+		}
+
 
 	}
 
 	@Override
 	public WithdrawInfo[] getWithdrawInfos() throws Exception {
-		return new WithdrawInfo[0];
+		return selectAll(getConnection(P2PConst.DB_USER),
+				(re) -> {
+					ArrayList<WithdrawInfo> list = new ArrayList<>();
+					while (re.next()) {
+						WithdrawInfo e = new WithdrawInfo();
+
+						e.id = re.getLong(1);
+						e.userId = re.getLong(2);
+						e.bid = re.getInt(3);
+						e.amount = re.getBigDecimal(4);
+						e.charge = re.getBigDecimal(5);
+						e.address = re.getString(6);
+
+						list.add(e);
+					}
+
+					return list.size() == 0 ? null : list.toArray(new WithdrawInfo[list.size()]);
+				},
+				"SELECT A.F01, A.F02 USER_ID, A.F03 BID A.F05 TRANS_AMOUNT, A.F06 CHARGE, B.F05 ADDRESS "
+						+ "FROM T6028 A, T6012 B WHERE A.F02 = B.F02 AND A.F03 = B.F03 AND A.F04 = B.F01 "
+						+ "AND A.F10 = 'SHTG' AND A.F08 = 'F' AND A.F02 = ? ", BID
+		);
 	}
 
 	@Override
 	public void withdraw(WithdrawInfo info) throws Exception {
 
+		// 交易状态： ZCCG 转出成功; ZCSB 转出失败
+		String state = "ZCSB";
+		String hash = "";
+		BigDecimal transAmount = BigDecimal.ZERO;
+
+		try {
+			// 获取平台对应提币钱包信息
+			WalletEntity we = this.getOutWaletInfo();
+
+			KeyPair source = KeyPair.fromSecretSeed(MyCrypt.myDecode(we.privateKey));
+
+			KeyPair dest = KeyPair.fromAccountId(info.address);
+			xlmServer.accounts().account(dest);
+
+			// 查询平台提币钱包信息
+			AccountResponse sourceAccount = xlmServer.accounts().account(source);
+
+			BigDecimal balance = this.getXlmBalance(sourceAccount);
+
+			BigDecimal canTransAmount = balance.subtract(CHANGE_FEE).subtract(MIN_BALANCE);
+
+			if (canTransAmount.compareTo(info.amount) < 0) {
+				throw new Exception("平台提币钱包余额不足，提币失败！");
+			}
+
+			String amount = XLM_FORMAT.format(info.amount);
+			Transaction transaction = new Transaction.Builder(sourceAccount)
+					.addOperation(new PaymentOperation.Builder(dest, new AssetTypeNative(), amount).build())
+					.build();
+			transaction.sign(source);
+
+			SubmitTransactionResponse response = xlmServer.submitTransaction(transaction);
+			if (response.isSuccess()) {
+				hash = response.getHash();
+				state = "ZCCG";
+				transAmount = info.amount;
+			} else {
+				hash = "提币失败：" + response.getExtras().getResultCodes().getTransactionResultCode();
+			}
+		} catch (Exception e) {
+			hash = e.getMessage().substring(0,200);
+		}
+
+		// 更新提币记录
+		execute(getConnection(P2PConst.DB_USER),
+				"UPDATE T6028 SET F10 = ?, F13 = ?, F14 = CURRENT_TIMESTAMP(), F16 = ? WHERE F01 = ? ",
+				state, info.userId, hash, info.id
+		);
+
+		// 更新用户资产
+		if (transAmount.compareTo(BigDecimal.ZERO) > 0) {
+			updateUserAsset(info.userId, info.bid, info.amount, info.charge, TRADE.TRANS_OUT);
+		}
+	}
+
+	/**
+	 * 获取平台对应提币钱包信息
+	 *
+	 * @return wallet entity
+	 * @throws Exception
+	 */
+	private WalletEntity getOutWaletInfo() throws Exception {
+		return select(getConnection(P2PConst.DB_CONSOLE),
+				(re) -> {
+					WalletEntity t = new WalletEntity();
+
+					if (re.next()) {
+						t.id = re.getInt(1);
+						t.bid = re.getInt(2);
+						t.address = re.getString(3);
+						t.privateKey = re.getString(4);
+						t.ip = re.getString(5);
+						t.port = re.getString(6);
+						t.serverName = re.getString(7);
+						t.serverPasswd = re.getString(8);
+					}
+
+					return t;
+				},
+				"SELECT F01,F02,F03,F04,F05,F06,F07,F08 FROM T7103 WHERE F02 = ?", BID
+		);
 	}
 
 	/**
@@ -231,7 +378,7 @@ public class XlmManageImpl extends AbstractManageService implements XlmManage {
 	}
 
 	/**
-	 * 更新并记录用户资产变动
+	 * 更新并记录用户资产变动日志
 	 *
 	 * @param userId   用户ID
 	 * @param bid      数字币ID
@@ -267,7 +414,7 @@ public class XlmManageImpl extends AbstractManageService implements XlmManage {
 			);
 
 			String content = String.format("尊敬的用户，您于%s转入%s枚%s，感谢您的使用。", tradeTime, XLM_FORMAT.format(amount), BJC);
-			sms(userId, "转入ONT", content);
+			sms(userId, "转入ONT成功", content);
 		} else {
 			// 更新用户资产
 			execute(getConnection(P2PConst.DB_USER),
@@ -295,6 +442,14 @@ public class XlmManageImpl extends AbstractManageService implements XlmManage {
 
 	}
 
+	/**
+	 * 站内消息发送
+	 *
+	 * @param userId    用户
+	 * @param title     消息标题
+	 * @param content   消息内容
+	 * @throws Exception
+	 */
 	private void sms(long userId, String title, String content) throws Exception {
 		execute(getConnection(P2PConst.DB_USER),
 				"INSERT INTO T6100 SET F02=?,F03=?,F04=?,F05=?,F06=CURRENT_TIMESTAMP()",
